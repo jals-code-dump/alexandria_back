87151cbc34929a412a2bf3176223caac8349b26d
jals-code-dump <62021119+jals-code-dump@users.noreply.github.com>
2021-08-09 20:11:31 +0100

    created empty class for developing
    created the nlp_tools class that i'll be working on

---
+++ nlp-tools/nlp_tools.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains all the NLP Tools required for processing text files.
+
+Created on Mon Aug  9 18:56:33 2021
+
+@author: Alex "Jal" Counihan
+"""
+
+class nlp_tools():
+    """
+    """
+    def __init__(self):
+        # initialisation to be stored here
+        pass
+
+    def pdf_handler(self):
+        # deal with pdf files
+        pass
+
+    def readability(self):
+        # return readaility score
+        pass
+
+    def tag(self):
+        # auto tag based on themes
+        pass
+
+    def gender_neutral(self):
+        # checks for gender neutral terminology
+        pass
+
+    def acronym_check(self):
+        #checks for mad acronyms
+        pass
+
+
+# testing function - only runs at module execution
+if __name__ == '__main__':
+    pass
